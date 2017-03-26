ad348d0c905efaaeac5e9d2182f8ad45ff963507
Nick Felker <handnf@gmail.com>
2017-03-26 16:16:14 -0400

    Moves scripts to separate files

---
+++ controller.js
@@ -0,0 +1,162 @@
+function dateSort(a, b) {
+    return new Date(b.date) - new Date(a.date);
+}
+
+function alphaSort(map) {
+    keys = [];
+    for (i in map) {
+        keys.push(i);   
+    }
+    return keys.sort();
+}
+
+function populateMenu(categories) {
+    var output = "<li class='filter' onclick='populateTimeline()'>All</li>";
+    console.log(categories);
+    var keys = alphaSort(categories);
+    console.log(keys);
+    for (i in keys) {
+        output += "<li class='filter' onclick='populateTimeline(\"" + keys[i] + "\")'>" + keys[i] + "</li>";
+    }
+    output += "<li class='filter' onclick='contactDialog()'>Contact</li>";
+    document.getElementById('links').innerHTML = output;
+}
+
+function useImage(type) {
+    var icon = "icons/checkbox-blank-circle.svg";
+    var color = "#4CAF50";
+    if (type == "Books") {
+        icon = "icons/library.svg";
+    } else if (type == "Articles") {
+        icon = "icons/newspaper.svg";
+    } else if (type == "Hacks") {
+        icon = "icons/code-tags.svg";
+    } else if (type == "Apps") {
+        icon = "icons/code-tags.svg";
+    } else if (type == "Education") {
+        icon = "icons/school.svg";
+    } else if (type == "Publications") {
+        icon = "icons/book.svg";
+    } else if (type == "Employment") {
+        icon = "icons/briefcase.svg";
+    } else if (type == "Awards") {
+        icon = "icons/ribbon.svg";
+    } else if (type == "Events") {
+        icon = "icons/calendar.svg";
+    }
+    var output = "<div class='cd-timeline-img cd-picture' style='background-color: " + color + "'>";
+    output += "<img src='" + icon + "' alt='Picture'>  </div>";
+    return output;
+}
+
+function getUrl(url, text) {
+    return "<a href='" + url + "' target='_blank' class='cd-read-more'>" + text + "</a>";
+}
+
+function populateTimeline(filter) {
+    // Reset
+    rehide();
+    document.getElementById('cd-timeline').innerHTML = "";
+
+    // Sort by date
+    me.sort(dateSort);
+    // Current date
+    currDate = "";
+    // Query categories
+    categories = [];
+    var count = 0;
+
+    for (var index = 0; index < me.length; index++) {
+        var item = me[index];
+        categories[item.type] = item.type;
+
+        if ((filter !== undefined && item.type == filter) 
+                || filter === undefined) {
+            // Check type
+            var output = "<div class='cd-timeline-block'>";
+            output += useImage(item.type);
+            output += "<div class='cd-timeline-content'><p>";
+            if (item.type == "Books") {
+                output += "<h2>" + item.name + "</h2>";
+                if (item.series !== undefined) {
+                    output += "<h3>" + item.series + " #" + item.seriesNo + "</h3>"; 
+                }
+            } else if (item.type == "Publications") {
+                output += "<h2>" + item.name + "</h2>";
+                output += "<em>" + item.publishedAt + "</em>";
+            } else if (item.type == "Hacks") {
+                output += "<h2>" + item.name + "</h2>";
+                if (item.purpose !== undefined) {
+                    output += "<h3>Built at " + item.purpose + "</h3>";
+                }
+                if (item.acolades !== undefined) {
+                    output += "<em>" + item.acolades + "</em>";   
+                }
+            } else if (item.type == "Articles") {
+                output += "<h2>" + item.name + "</h2>";
+            } else if (item.type == "Education") {
+                output += "<h2>" + item.name + "</h2>";
+                if (item.gpa !== undefined) {
+                    output += "<h3>" + item.gpa + " GPA</h3>";
+                }
+            } else if (item.type == "Employment") {
+                output += "<h2>Started Job at " + item.name + "</h2>";
+                output += "<h3>" + item.position + "</h3>";
+                output += "<h4>" + item.date + " - " + item.ended + "</h4>";
+            } else if (item.type == "Awards") {
+                output += "<h2>" + item.award + "</h2>";
+                output += "<h3>" + item.name + "</h3>";
+            } else if (item.type == "Events") {
+                output += "<h2>Hosted " + item.name + "</h2>";
+                output += "<h3>" + item.specificDate + "</h3>";
+                output += "<h3>" + item.location + "</h3>";
+            } else if (item.type == "News") {
+                output += "<h2>" + item.name + "</h2>";
+                output += "<h3>" + item.publication + "</h3>";
+            } else {
+                output += "<h2>" + item.name + "</h2>";
+            }
+            output += "</p>";
+            if (item.link !== undefined) {
+                output += getUrl(item.link, item.linkText);
+            }
+            if (currDate != item.date) {
+                output += "<span class='cd-date'>" + item.date + "</span>";   
+                currDate = item.date;
+            }
+            output += "</div></div>";
+            document.getElementById('cd-timeline').innerHTML += output;
+            count++;
+            document.getElementById('view_x_items').innerHTML = count + ((count == 1) ? " Item" : " Items");
+        }
+    }
+    rehide();
+    $('#contact-me').fadeOut(400);
+    $('#cd-timeline').fadeIn(400);
+    return categories;
+}
+
+function contactDialog() {       
+    // Generate things
+    var output = "<ul>";
+    for (var i = 0; i < contact.length; i++) {
+        var item = contact[i];
+        if (item.icon !== undefined) {
+            output += "<li><a target='_blank' href='" + item.link + "'><img src='" + item.icon + "' alt='" + item.type + "' /></a></li>"
+        } else {
+            output += "<li><a target='_blank' href='" + item.link + "'>" + item.type + "</a></li>";
+        }
+    }
+    output += "</ul>";
+    document.getElementById('contact-me').innerHTML = output;
+
+    var count = contact.length; 
+    document.getElementById('view_x_items').innerHTML = count + ((count == 1) ? " Item" : " Items");
+    $('#contact-me').fadeIn(400);
+    $('#cd-timeline').fadeOut(400);
+}
+
+window.onload = function() {
+    categories = populateTimeline();
+    populateMenu(categories);
+};
---
+++ items.js
@@ -0,0 +1,199 @@
+/* My data 
+ * 
+ * This will be a JSON object so I can easily populate different fields.
+ * Eventually this will move to a separate file.
+ * It may be useful to develop certain types of schema.
+ */
+/* Some Constants */
+var READ_FULL_TEXT = "Read Full Text";
+var VIEW_DEVPOST = "View DevPost Submission";
+var READ_ARTICLE = "Read Article";
+
+me = [
+    {
+        type: "Publications",
+        name: "Using Wearable Sensors to Collect User Data at an Event as a Live API and for Logistical Planning",
+        publishedAt: "Sensor Applications Symposium 2017, Rowan University",
+        link: "https://www.researchgate.net/publication/312670816_Using_Wearable_Sensors_to_Collect_User_Data_at_an_Event_as_a_Live_API_and_for_Logistical_Planning",
+        linkText: READ_FULL_TEXT,
+        date: "March 2017"
+    },
+    {
+        type: "Publications",
+        name: "Building MemSat: A CubeSat for Testing Resistive Memory",
+        publishedAt: "Sensor Applications Symposium 2017, Rowan University",
+        linkText: READ_FULL_TEXT,
+        link: "https://www.researchgate.net/publication/315063307_Building_MemSat_A_CubeSat_for_Testing_Resistive_Memory",
+        date: "March 2017",
+    },
+    {
+        type: "Books",
+        name: "The Lost Hunter",
+        series: "The Legion of Supervillans",
+        seriesNo: "1",
+        bookCover: "",
+        link: "https://www.amazon.com/Lost-Hunter-Journal-Legion-Supervillains-ebook/dp/B06XMX9LNW/ref=sr_1_1?ie=UTF8&qid=1489109101&sr=8-1&keywords=nick+felker",
+        linkText: "Purchase eBook for $3",
+        date: "February 2017"
+    }, {
+        type: "Articles",
+        name: "Adding TV Channels to Your App With the TIF Companion Library",
+        link: "https://android-developers.googleblog.com/2016/11/adding-tv-channels-to-your-app-with-the-tif-companion-library.html",
+        linkText: "Read at Android Developers Blog",
+        date: "November 2016"
+    }, {
+        type: "Articles",
+        name: "Started writing for Android TV News",
+        link: "http://androidtv.news",
+        linkText: "Read at Android TV News",
+        date: "September 2015"
+    }, {
+        type: "Hacks",
+        name: "Smart Tattoos",
+        purpose: "PennApps XV",
+        acolades: "Grand Prize, Best Hardware Hack, Best Use of Rapid Prototyping",
+        link: "https://devpost.com/software/smart-tattoos",
+        linkText: VIEW_DEVPOST,
+        date: "January 2017"
+    }, {
+        type: "Hacks",
+        name: "RedSi",
+        purpose: "PennApps XIV",
+        acolades: "Best Education Hack",
+        link: "https://devpost.com/software/redsi",
+        linkText: VIEW_DEVPOST,
+        date: "September 2016"
+    }, {
+        type: "Hacks",
+        name: "Cogni Journal",
+        purpose: "PennApps XIII",
+        link: "https://devpost.com/software/cogni-journal",
+        linkText: VIEW_DEVPOST,
+        date: "January 2016"
+    }, {
+        type: "Hacks",
+        name: "Pyed Piper",
+        purpose: "DragonHacks 2016",
+        acolades: "Best Design, Best Domain Name",
+        link: "https://devpost.com/software/pyed-piper",
+        linkText: VIEW_DEVPOST,
+        date: "January 2016"
+    }, {
+        type: "Hacks",
+        name: "PetApps",
+        purpose: "PennApps XII",
+        link: "https://devpost.com/software/petapps",
+        linkText: VIEW_DEVPOST,
+        date: "September 2015"
+    }, {
+        type: "Hacks",
+        name: "Munch",
+        purpose: "IEEEMADC 2015",
+        acolades: "Best Mobile App Design",
+        link: "https://devpost.com/software/munch-nod0rf",
+        linkText: VIEW_DEVPOST,
+        date: "May 2015"
+    }, {
+        type: "Hacks",
+        name: "nVader",
+        purpose: "Philly Codefest 2015",
+        link: "https://devpost.com/software/nvader-qaopxi",
+        linkText: VIEW_DEVPOST,
+        date: "March 2015"
+    }, {
+        type: "Hacks",
+        name: "The Light Painter's Palette",
+        purpose: "Dragon Hacks 2015",
+        link: "https://devpost.com/software/the-light-painter-s-palette-q5zrh",
+        acolades: "2nd Place",
+        linkText: VIEW_DEVPOST,
+        date: "January 2015"
+    }, {
+        type: "Education",
+        name: "Graduated Glassboro High School",
+        gpa: "4.0",
+        date: "June 2013"
+    }, {
+        type: "Employment",
+        name: "Google",
+        position: "Developer Relations Intern, Android TV & IoT",
+        ended: "August 2016",
+        date: "May 2016"
+    }, {
+        type: "Employment",
+        name: "Rowan University",
+        position: "Web Development for College of Science and Math & College of Engineering",
+        ended: "Present",
+        date: "August 2015"
+    }, {
+        type: "Employment",
+        name: "The Chef's Kitchen",
+        position: "Web Developer",
+        ended: "September 2015",
+        date: "June 2015"
+    }, {
+        type: "Awards",
+        name: "IEEE Sumo Robotics Kit Competition",
+        award: "2nd Place",
+        date: "September 2014"
+    }, {
+        type: "Awards",
+        name: "Troop 123",
+        award: "Eagle Scout",
+        date: "June 2013"
+    }, {
+        type: "Events",
+        name: "Student Activities Conference, 2014",
+        location: "Rowan University",
+        specificDate: "April 4-6",
+        date: "April 2014"
+    }, {
+        type: "Events",
+        name: "ProfHacks 2K16",
+        location: "Rowan University",
+        specificDate: "March 12-13",
+        date: "March 2016"
+    }, {
+        type: "News",
+        name: "Bowe School students explore hands-on engineering",
+        publication: "NJ.com",
+        link: "http://www.nj.com/south-jersey-towns/index.ssf/2017/03/bowe_school_students_explore_h.html",
+        linkText: READ_ARTICLE,
+        date: "March 2017"
+    }, {
+        type: "News",
+        name: "LIVE BLOG: PROFHacks 2017 pits students against each other in &#39;hackathon&#39;",
+        publication: "The Whit",
+        link: "http://thewhitonline.com/2017/02/news/live-blog-profhacks-2017-pits-students-against-each-other-in-hackathon/",
+        linkText: READ_ARTICLE,
+        date: "February 2017"
+    }, {
+        type: "News",
+        name: "Rowan University&#39;s first hackathon is focused on the physical web",
+        publication: "Technically Philly",
+        link: "http://technical.ly/philly/2016/03/10/rowan-university-hackathon/",
+        linkText: READ_ARTICLE,
+        date: "March 2016"
+    }, {
+        type: "News",
+        name: "Students create technological tools at Rowan hackathon",
+        publication: "Courier Post",
+        link: "http://www.courierpostonline.com/story/news/2016/03/13/rowan-engages-in-first-ever-hackathon/81581144/",
+        linkText: READ_ARTICLE,
+        date: "March 2016"
+    }, {
+        type: "News",
+        name: "Student computer whizzes compete at PennApps Hackathon",
+        publication: "The Philadelphia Inquirer",
+        link: "http://www.philly.com/philly/news/20150906_Student_computer_whizzes_compete_at_PennApps_Hackathon.html",
+        linkText: READ_ARTICLE,
+        date: "September 2015"
+    }, {
+        type: "News",
+        name: "Glassboro High School to present &#39;Beauty and the Beast&#39;",
+        publication: "NJ.com",
+        link: "http://www.nj.com/gloucester-county/index.ssf/2012/02/glassboro_high_school_to_prese_1.html",
+        linkText: READ_ARTICLE,
+        date: "February 2012"
+    }
+];
