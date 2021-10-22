505f855bc3550ea3bb884477ac97d5910fedc9cb
Fleker <handnf@gmail.com>
2021-10-22 19:09:59 +0000

    Deploying to gh-pages from @ Fleker/Fleker.github.io@c3825b20e51b7c68d5ae05eb8c71439f0ced87cc 🚀

--- src/sorts.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntry } from "./entries";
-
-export function dateSort(a: BaseEntry, b: BaseEntry) {
-  return new Date(b.date).getTime() - new Date(a.date).getTime()
-}
-
-export function alphaSort(map: Record<string, BaseEntry>) {
-  return Object.keys(map).sort();
-}
